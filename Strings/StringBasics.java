17b61b8824bda1257f5673f8d8ff3b4d1e4f6f25
Sandeep Moorani <sandeepmoorani222@gmail.com>
2023-03-13 23:46:14 +0500

    second commit

--- Strings/StringBasics.java
+++ Strings/StringBasics.java
@@ -21,3 +21,14 @@ public class StringBasics {
         System.out.println(fullName.charAt(0));
     }
 }
+
+// git queries
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin git@github.com:Sandeepmoorani/Placement.git
+// git push -u origin main
+// git remote add origin git@github.com:Sandeepmoorani/Placement.git
+// git branch -M main
+// git push -u origin main
