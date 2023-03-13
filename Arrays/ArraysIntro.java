3c9fd82d036e57266fed69514289d5d73c3b7635
Sandeep Moorani <sandeepmoorani222@gmail.com>
2023-03-14 00:06:00 +0500

    first commit

---
+++ Arrays/ArraysIntro.java
@@ -0,0 +1,22 @@
+package Arrays;
+
+public class ArraysIntro {
+    public static void main(String args[]) {
+        //creating an array 
+
+        int marks[] = new int[50];
+
+        int numbers[] = {1, 2, 3};
+
+        int moreNumbers[] = {4, 5, 6};
+
+        String fruits[] = {"Banana", "Cheku", "orange"};
+
+        // System.out.println(marks[0]);
+        // System.out.println(numbers[0]);
+        // System.out.println(moreNumbers[0]);
+        System.out.println(fruits[0]);
+        System.out.println(fruits[1]);
+        System.out.println(fruits[2]);
+    }
+}
